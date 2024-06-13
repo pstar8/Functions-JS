7e3e05f42c0ef7b1c556ff681350a10077bc4f25
pstar8 <ifedaraniyi@gmail.com>
2024-06-13 11:41:58 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// 1.
+function myName() {
+  let name = 'Ifeoluwa';
+  console.log(name);
+}
+myName();
+
+// 2.
+favoriteFoods = ['pizza', 'ice-cream'];
+
+//3.
+function randomFood() {}
+
+//4.
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//5.
+function displayOddNumbers() {}
+
+//6.
+function displayEvenNumbers() {}
+
+//7.
+function returnFirstOddNumber() {
+  numbers.pop();
+  let poppedVal = numbers.pop;
+  console.log('poppedVal');
+  return poppedVal;
+}
+
+//8.
+function returnFirstEvenNumber() {}
